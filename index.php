f1e72275db2e1fa76e211d7dce424fe99018a0e3
kamalkant-0013 <kamalkant.strix@gmail.com>
2026-04-21 11:40:40 +0530

    update

--- index.php
+++ index.php
@@ -171,6 +171,136 @@ var_dump($a===$b);
 echo "<br>";
 var_dump($a<>$b);
 echo "<br>";
-var_dump($a!==$b);
+var_dump($a == $b);
 
+echo "<br>";
+echo "<h2>if condition</h2>";
+$a = "kamal kant";
+if ($a == "kamal kant") {
+    echo "Hello my name is kamal kant";
+}
+
+echo "<br>";
+echo "<h2>else condition</h2>";
+
+$a = 45;
+if ($a == 21) {
+    echo "how are you";
+}else {
+    echo "why are you hare ?";
+}
+
+echo "<br>";
+echo "<h2>elseif condition</h2>";
+
+$a = 45;
+if ($a == 21) {
+    echo "how are you";
+}elseif ($a == 45) {
+    echo "why are you hare ?";
+}else {
+    echo "where are you from";
+}
+
+echo "<br>";
+echo "<h2>shorthand if condition</h2>";
+
+echo "<br>";
+$a = 47;
+
+if ($a <= 50 ) $b = "i am learning PHP";
+echo $b;
+
+echo "<br>";
+echo "<h2>Nestedif condition</h2>";
+$a = "kamal";
+
+if ($a == "nikhil") {
+    echo "i am nikhil";
+}if ($a == "kamal"){
+    echo "I Am Kamal Kant";
+}else{
+    echo "i am a student";
+}
+
+echo "<br>";
+echo "<h2>to find number is positive or nagtive</h2>";
+echo "<br>";
+$a = -1;
+if ($a > 0) {
+    echo "Number is positive";
+}elseif ($a < 0) {
+    echo "number is negative ";
+}else {
+    echo "number is zero";
+}
+
+echo "<br>";
+echo "<h2>program to find eligible age for voting</h2>";
+echo "<br>";
+$a = 18;
+if ($a >= 18) {
+    echo "you are aligible for voting";
+}
+elseif ($a < 18) {
+    echo "you are not eligible for voting";
+}
+echo "<br>";
+echo "<h2>program to find which number is greater</h2>";
+echo "<br>";
+$a = 88;
+$b = 54;
+if ($a > $b) {
+    echo "A is greater";
+}else {
+    echo "B is greater";
+}
+echo "<br>";
+echo "<h2>Largest of three numbers</h2>";
+echo "<br>";
+$a = 8;
+$b = 7;
+$c = 55;
+
+if ($a >= $b && $a >= $c) {
+    echo "$a is largest";
+}elseif ($b >= $a && $b >= $c) {
+    echo "$b is largest";
+}else {
+    echo "$c is largest";
+}
+
+echo "<br>";
+echo "<h2>program to assign grades</h2>";
+echo "<br>";
+$a = 29;
+
+if ($a >= 90) {
+    echo "you got grade A+";
+}elseif ($a >= 75) {
+    echo "you got grade A";
+}elseif ($a >= 60) {
+    echo "you got grade B";
+}elseif ($a >= 33) {
+    echo "you got passing marks";
+}else {
+    echo "you are fail <br> Better Luck next time";
+}
+
+echo "<br>";
+echo "<h2>to print login</h2>";
+echo "<br>";
+
+$username = "kamal kant";
+$password = 158;
+
+if ($username == "kamal kant" && $password == 1458) {
+    echo "login sucessfull";
+}elseif ($username != "kamal kant" && $password == 1458) {
+    echo "username is incorrect";
+}elseif ($username == "kamal kant" && $password != 1458) {
+    echo "password is incorrect";
+}elseif ($username != "kamal kant" && $password != 1458) {
+    echo "username and password does not match";
+}
 ?>
